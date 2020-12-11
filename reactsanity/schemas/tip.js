e5d528874fc84dfaa3a7b2b722eff9eb37b0f48e
Reggio <contact@graphandco.com>
2020-12-11 20:10:45 +0100

    Add tips schema

---
+++ reactsanity/schemas/tip.js
@@ -0,0 +1,31 @@
+export default {
+    name: 'tip',
+    tip: 'Tip',
+    type: 'document',
+    fields: [
+        {
+            name: 'title',
+            type: 'string',
+        },
+        {
+            name: 'description',
+            type: 'text',
+        },
+        {
+            name: 'code',
+            type: 'text',
+        },
+        {
+            name: "language",
+            title: 'Langage',
+            type: 'string',
+            options: {
+                list: [
+                    { value: 'css', title: 'CSS' },
+                    { value: 'javascript', title: 'Javascript' },
+                ]
+            }
+        }
+    ]
+  }
+  
